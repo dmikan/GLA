d4cbba5162764606ec02ec19f0994ee97f48e13a
Danny Julián Perilla Mikán <dmikan@id.uff.br>
2026-03-01 09:01:47 -0500

    feat: Introduce a Streamlit component for manual data input, allowing users to define well dimensions, input field, WCT, and injection/production data, and save it to a CSV file.

--- app/components/file_upload/manual_input_component.py
+++ app/components/file_upload/manual_input_component.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+
 class ManualInputComponent:
     def __init__(self, tmp_dir):
         self.tmp_dir = tmp_dir
@@ -22,86 +23,123 @@ class ManualInputComponent:
         self.final_df = None
 
     def load(self):
-        """
-        Load the manual input component.
-        """
         self._choose_dimension()
-        self._create_labels()
-        self._create_input_empty_dataframe()
+        self._create_labels_list()
+        self._initialize_and_update_dataframes()
         self._show_and_fill_manual_input_dataframes()
         self._build_dataframe_to_optimizer()
         self._manual_input_persistence()   
 
-
     def _choose_dimension(self):
         """
-        Choose the dimension of the manual input.
+        This method is used to choose the dimension of the manual input dataframes.
         """
         with st.expander("⚙️ Configuration Settings", expanded=True):
             col1, col2 = st.columns(2)
             with col1:
                 self.num_wells = st.number_input("Number of wells", min_value=1, max_value=100, value=5, step=1)
-            
             with col2:
                 self.num_rows = st.number_input("Number of data rows", min_value=1, max_value=100, value=5, step=1)
         return True
 
-
-    def _create_labels(self):
+    def _create_labels_list(self):
         """
-        Create the labels for the manual input.
+        This method is used to create the labels for the manual input dataframes.
         """
         self.field_wells_labels = ["field"] + [f"well_{i+1}" for i in range(self.num_wells)]
         self.wct_labels = [f"Well_{i+1}_wct" for i in range(self.num_wells)]
         self.injection_production_labels = [item for i in range(1, self.num_wells + 1) for item in (f"Well_{i}_inj", f"Well_{i}_fluid")]
         return True
 
-
-    def _create_input_empty_dataframe(self):
+    def _build_empty_dataframe(self, labels_list, single_row_flag):
         """
-        Create an empty dataframe for manual input.
+        This method is used to build an empty dataframe with the given labels.
         """
-        self.field_wells_df = pd.DataFrame(
-            [["" for _ in self.field_wells_labels]],
-            columns=self.field_wells_labels,
-            index=[1]
+        if single_row_flag:
+            return pd.DataFrame([["" for _ in labels_list]], 
+                                columns=labels_list, 
+                                index=[1])
+                                
+        return pd.DataFrame([["" for _ in labels_list] for _ in range(self.num_rows)],
+                            columns=labels_list,
+                            index=range(1, self.num_rows + 1)
         )
 
-        self.wct_df = pd.DataFrame(
-            [["" for _ in self.wct_labels]],
-            columns=self.wct_labels,
-            index=[1]
-        )
-        
-        self.injection_production_df = pd.DataFrame(
-            [["" for _ in self.injection_production_labels] for _ in range(self.num_rows)],
-            columns=self.injection_production_labels,
-            index=range(1, self.num_rows + 1)
-        )
+    def _merge_with_saved(self, new_dataframe, session_key_filled_dataframe):
+        """
+        This method is used to merge the new empty dataframe with the saved dataframe.
+        """
+        saved_dataframe = st.session_state.get(session_key_filled_dataframe)
+        if saved_dataframe is None:
+            return new_dataframe
+        new_dataframe.update(saved_dataframe)
+        return new_dataframe
+
+    def _flush_editor_to_session(self, editor_key_prefix, session_key_filled_dataframe, labels_list, single_row_flag):
+        """
+        This method is used to flush the editor state to the session state.
+        """
+        prev_num_wells = st.session_state.get("prev_num_wells", self.num_wells)
+        # this is the editor state of changes made by the user
+        changes_editor_state: dict = st.session_state.get(f"{editor_key_prefix}_{prev_num_wells}") 
+        if changes_editor_state is not None:
+            saved_df = st.session_state.get(session_key_filled_dataframe)      
+            changes_df = pd.DataFrame(changes_editor_state.get("edited_rows", {})).T
+            changes_df.index = changes_df.index.astype(int) + 1
+            saved_df.update(changes_df)
+            st.session_state.session_key_filled_dataframe = saved_df
 
 
-        if  st.session_state.get('field_wells_df') is not None:
-            self.field_wells_df[st.session_state.field_wells_df.columns] = st.session_state.field_wells_df
-        if  st.session_state.get('wct_df') is not None:
-            self.field_wells_df[st.session_state.wct_df.columns] = st.session_state.wct_df
-        if  st.session_state.get('injection_production_df') is not None:    
-            self.field_wells_df[st.session_state.injection_production_df.columns] = st.session_state.injection_production_df
+
+    def _initialize_and_update_dataframes(self):
+        """
+        This method is used to initialize the dataframes.
+        """
+        self._flush_editor_to_session(editor_key_prefix="field_wells_editor", 
+                                    session_key_filled_dataframe="field_wells_df", 
+                                    labels_list=self.field_wells_labels, 
+                                    single_row_flag=True)
+                                    
+        self._flush_editor_to_session(editor_key_prefix="wct_editor", 
+                                    session_key_filled_dataframe="wct_df", 
+                                    labels_list=self.wct_labels, 
+                                    single_row_flag=True)
+                                    
+        self._flush_editor_to_session(editor_key_prefix="prod_editor", 
+                                    session_key_filled_dataframe="injection_production_df", 
+                                    labels_list=self.injection_production_labels, 
+                                    single_row_flag=False)
 
 
-        if st.session_state.get('field_wells_df') is None:
-            st.session_state.field_wells_df = self.field_wells_df
-        if st.session_state.get('wct_df') is None:
-            st.session_state.wct_df = self.wct_df
-        if st.session_state.get('injection_production_df') is None:
-            st.session_state.injection_production_df = self.injection_production_df
 
-            
+
+        self.field_wells_df = self._build_empty_dataframe(labels_list=self.field_wells_labels, 
+                                                        single_row_flag=True)
+
+        self.wct_df = self._build_empty_dataframe(labels_list=self.wct_labels, 
+                                                        single_row_flag=True)
+                                                        
+        self.injection_production_df = self._build_empty_dataframe(labels_list=self.injection_production_labels, 
+                                                        single_row_flag=False)
+
+
+
+
+        self.field_wells_df = self._merge_with_saved(new_dataframe=self.field_wells_df, 
+                                                    session_key_filled_dataframe="field_wells_df")
+
+        self.wct_df = self._merge_with_saved(new_dataframe=self.wct_df, 
+                                            session_key_filled_dataframe="wct_df")
+
+        self.injection_production_df = self._merge_with_saved(new_dataframe=self.injection_production_df, 
+                                                            session_key_filled_dataframe="injection_production_df")
+
+
         return True
-
 
     def _show_and_fill_manual_input_dataframes(self):
         """
-        Show the manual input widget.
+        This method is used to show and fill the manual input dataframes.
         """
         st.divider() 
         st.subheader("📋 Information of Fields and Wells")
@@ -111,30 +149,28 @@ class ManualInputComponent:
         info_header.update({col: st.column_config.TextColumn(f"🛢️ {col.replace('_', ' ').title()}") for col in self.field_wells_labels[1:]})
 
         self.filled_field_wells_df = st.data_editor(
-            st.session_state.field_wells_df,
+            self.field_wells_df,
             column_config=info_header, 
             num_rows="fixed",
             hide_index=True, 
             use_container_width=True,
-            key="field_wells_editor"
+            key=f"field_wells_editor_{self.num_wells}"
         )
 
-        # WCT Input
         st.divider() 
         st.subheader("📋 Information of WCT")
         st.caption("Please define the wct for each well below.")
         
         wct_header = {col: st.column_config.TextColumn(f"📉 {col.replace('_', ' ')}") for col in self.wct_labels}
         self.filled_wct_df = st.data_editor(
-            st.session_state.wct_df,
+            self.wct_df,
             column_config=wct_header, 
             num_rows="fixed",
             hide_index=True, 
             use_container_width=True,
-            key="wct_editor"
+            key=f"wct_editor_{self.num_wells}"
         )
 
-        # Production Data Input
         st.divider() 
         st.subheader("📊 Production Data Input")
         st.caption("Enter the daily injection rates and fluid types for the simulation.")
@@ -142,49 +178,47 @@ class ManualInputComponent:
         prod_header = {col: st.column_config.TextColumn(f"{col.replace('_', ' ')}") for col in self.injection_production_labels}
         
         self.filled_injection_production_df = st.data_editor(
-            st.session_state.injection_production_df, # has to be dependent of the session state
+            self.injection_production_df, 
             column_config=prod_header,  
             num_rows="fixed",
             hide_index=True,
             use_container_width=True,
-            key="prod_editor"
+            key=f"prod_editor_{self.num_wells}"
         )
 
-        st.session_state.filled_field_wells_df = self.filled_field_wells_df
-        st.session_state.filled_wct_df = self.filled_wct_df
-        st.session_state.filled_injection_production_df = self.filled_injection_production_df
+        st.session_state.field_wells_df = self.filled_field_wells_df
+        st.session_state.wct_df = self.filled_wct_df
+        st.session_state.injection_production_df = self.filled_injection_production_df
+        st.session_state.prev_num_wells = self.num_wells
+        st.session_state.prev_num_rows = self.num_rows
         
         return True
 
-
     def _build_dataframe_to_optimizer(self):
+        """
+        This method is used to build the dataframe to the optimizer.
+        """
         try:
             total_columns = len(self.filled_injection_production_df.columns) + 1
             self.final_df = pd.DataFrame(columns=range(total_columns))
 
-            # Row 0: description
             self.final_df.loc[0] = ["description"] + [""] * (total_columns - 1)
 
-            # Row 1: well headers
             wells_header = self.filled_field_wells_df.columns.tolist()
             wells_header += [""] * (total_columns - len(wells_header))
             self.final_df.loc[1] = wells_header
 
-            # Row 2: well values
             wells_values = self.filled_field_wells_df.iloc[0].tolist()
             wells_values += [""] * (total_columns - len(wells_values))
             self.final_df.loc[2] = wells_values
 
-            # Row 3: wct values
             wct_values = ["wct"] + self.filled_wct_df.iloc[0].tolist()
             wct_values += [""] * (total_columns - len(wct_values))
             self.final_df.loc[3] = wct_values
 
-            # Row 4: data headers
             data_headers = ["index"] + self.filled_injection_production_df.columns.tolist()
             self.final_df.loc[4] = data_headers
 
-            # Following rows: production data
             for i, (idx, row) in enumerate(self.filled_injection_production_df.iterrows()):
                 row_values = [idx + 1] + row.tolist()
                 self.final_df.loc[5 + i] = row_values
@@ -195,6 +229,9 @@ class ManualInputComponent:
             return False
 
     def _manual_input_persistence(self):
+        """
+        This method is used to persist the manual input data.
+        """
         try:
             if self.final_df is not None and not self.final_df.empty and len(self.final_df) > 2:
                 df = self.final_df
@@ -211,8 +248,3 @@ class ManualInputComponent:
         except Exception as e:
             st.error(f"Error saving the data: {e}")
             return False
-
-
-
-
-
